7e788b093398ed160e14aa26c2ac211eea4ad3d3
Hoko Le <hokole527@gmail.com>
2019-10-31 16:00:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,9 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return "NO, NOT SINCE 1938!"
+    return 
   elsif phrase == phrase.upcase
-    return "I LOVE YOU TOO PUMPKIN!"
+    return "NO, NOT SINCE 1938!""I LOVE YOU TOO PUMPKIN!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
